aa907fbd6650fc98a65cfc661dc0e347f9cd13b6
Oğuzhan Aydınay <aydinayoguzhan@gmail.com>
2023-04-18 22:13:02 +0300

    Canlı yayın iframe ile eklendi

--- src/app/live-stream/live-stream.component.ts
+++ src/app/live-stream/live-stream.component.ts
@@ -7,11 +7,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./live-stream.component.scss']
 })
 export class LiveStreamComponent implements OnInit {
+  videoSrc: string = '';
 
   constructor(private navbarService:NavbarService) { }
 
   ngOnInit(): void {
     this.navbarService.changeActive(2);
+
+    const ws = new WebSocket(`ws://192.168.1.43:5001`);
+
+    ws.onmessage = (event) =>{
+      if (this.videoSrc === '') {
+        const blob = new Blob([event.data], {type: 'video/mp4'});
+        this.videoSrc = URL.createObjectURL(blob);
+        console.log(this.videoSrc)
+      }
+    }
   }
 
 }
