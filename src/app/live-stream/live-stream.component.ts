4f27bedd34da3a41f905f1cd8a24a9987c1a6bf4
Oğuzhan Aydınay <aydinayoguzhan@gmail.com>
2023-04-19 12:30:19 +0300

    Spinner eklendi

--- src/app/live-stream/live-stream.component.ts
+++ src/app/live-stream/live-stream.component.ts
@@ -1,5 +1,5 @@
 import { NavbarService } from './../../services/navbar/navbar.service';
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-live-stream',
@@ -8,21 +8,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LiveStreamComponent implements OnInit {
   videoSrc: string = '';
+  isIframeLoaded:boolean= false;
+
+  @ViewChild('iframe') iframe: ElementRef;
 
   constructor(private navbarService:NavbarService) { }
 
   ngOnInit(): void {
     this.navbarService.changeActive(2);
 
-    const ws = new WebSocket(`ws://192.168.1.43:5001`);
+    // const ws = new WebSocket(`ws://192.168.1.43:5001`);
 
-    ws.onmessage = (event) =>{
-      if (this.videoSrc === '') {
-        const blob = new Blob([event.data], {type: 'video/mp4'});
-        this.videoSrc = URL.createObjectURL(blob);
-        console.log(this.videoSrc)
-      }
-    }
+    // ws.onmessage = (event) =>{
+    //   if (this.videoSrc === '') {
+    //     const blob = new Blob([event.data], {type: 'video/mp4'});
+    //     this.videoSrc = URL.createObjectURL(blob);
+    //     console.log(this.videoSrc)
+    //   }
+    // }
+  }
+
+
+  onIframeLoad(){
+    this.isIframeLoaded = true;
   }
 
 }
